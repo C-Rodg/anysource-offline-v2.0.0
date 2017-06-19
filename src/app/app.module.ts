8a0dd30434d8af5638111ecdae8ca598c4427d9c
C-Rodg <rodgersbc@gmail.com>
2017-06-19 16:51:32 -0700

    Themed tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,38 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { CapturePage } from '../pages/capture/capture';
+import { ListPage } from '../pages/list/list';
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CapturePage,
+    ListPage,
+    SettingsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp, {
+      mode: 'md'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    CapturePage,
+    ListPage,
+    SettingsPage,
     TabsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/capture/capture.ts
+++ src/pages/capture/capture.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-capture',
+  templateUrl: 'capture.html'
 })
-export class AboutPage {
+export class CapturePage {
 
   constructor(public navCtrl: NavController) {
 
